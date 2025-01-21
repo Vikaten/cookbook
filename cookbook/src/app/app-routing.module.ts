41c4d6516feee0c4f7d1779659352ece4d8a872d
Temnova Vika <vikaten06@gmail.com>
2025-01-21 20:09:37 +0300

    feat: add button processing on the main screen and configure routing

--- cookbook/src/app/app-routing.module.ts
+++ cookbook/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MyRecipesComponent } from './my-recipes/my-recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'add-new-recipe', component: NewRecipeComponent },
+  { path: 'my-recipes', component: MyRecipesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
