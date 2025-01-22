7ac36b7e169619e9e7034747a029d62e26bb3caa
Temnova Vika <vikaten06@gmail.com>
2025-01-22 23:06:28 +0300

    fix: correct the addition of recipes to a new page

--- cookbook/src/app/data.service.ts
+++ cookbook/src/app/data.service.ts
@@ -13,11 +13,20 @@ export interface IRecipe {
   providedIn: 'root',
 })
 export class DataService {
+  constructor() {
+    const savedRecipes = localStorage.getItem('recipes');
+    if (savedRecipes) {
+      this.recipesArr = JSON.parse(savedRecipes);
+      this.recipesSubject.next(this.recipesArr); // Обновляем поток
+    }
+  }
   private recipesArr: IRecipe[] = [];
   private recipesSubject = new BehaviorSubject<IRecipe[]>(this.recipesArr);
+  savedRecipes = localStorage.getItem('recipes');
 
   saveData(newRecipe: IRecipe) {
     this.recipesArr.push(newRecipe);
+    localStorage.setItem('recipes', JSON.stringify(this.recipesArr));
     this.recipesSubject.next(this.recipesArr);
     console.log(this.recipesArr);
   }
